b11a34eebbc921e8b3d0b9bb1e0bf6703620a651
KevinBSC97 <kevinsotoamyor1@hotmail.com>
2023-08-03 17:31:59 -0500

    Se agrego variables de traduccion

--- src/app/View/Pages/cliente/cliente.component.ts
+++ src/app/View/Pages/cliente/cliente.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { MenuItem} from 'primeng/api';
 
 @Component({
@@ -9,51 +10,57 @@ import { MenuItem} from 'primeng/api';
 export class ClienteComponent implements OnInit {
   items!: MenuItem[];
   constructor(
+    private translate : TranslateService,
     private router : Router
   ) { }
 
   ngOnInit() {
     this.llenarItems();
+    this.subscribeToLanguageChange();
   }
 
-  llenarItems(){
-    this.items = [
-      {
-        label: 'X Opciones',
-        icon: 'pi pi-cog',
-        items: [
+  llenarItems() {
+    this.translate.get('cliente').subscribe((translations: any) => {
+      this.items = [
+        {
+          label: translations.opciones,
+          icon: 'pi pi-cog',
+          items: [
             {
-                label: 'X Comprar boletos',
-                icon: 'pi pi-refresh',
-                command: () => {
-                    console.log("Actualizo");
-                }
+              label: translations.comprar_boletos,
+              icon: 'pi pi-refresh',
             },
             {
-                label: 'X Historial de compras',
-                icon: 'pi pi-shopping-cart',
-                command: () => {
-                  console.log("Elimino");
-                }
+              label: translations.historial_compras,
+              icon: 'pi pi-shopping-cart',
             },
             {
-              label: 'X Editar perfil',
+              label: translations.editar_perfil,
               icon: 'pi pi-user-edit',
-              command: () => {
-                console.log("Elimino");
-              }
             },
             {
-              label: 'X Cerrar Sesión',
+              label: translations.cerrar_sesion,
               icon: 'pi pi-sign-out',
               command: () => {
-                console.log("Elimino");
                 this.cerrarSesion();
               }
             }
-        ]
-      }
-    ];
+          ]
+        }
+      ];
+    });
+  }
+
+
+    
+
+  subscribeToLanguageChange() {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      console.log(event)
+      // Cuando se produce un cambio en el idioma, volvemos a llenar los items
+      // para que las etiquetas se actualicen con las nuevas traducciones.
+      this.llenarItems();
+    });
   }
 
   cerrarSesion(){
